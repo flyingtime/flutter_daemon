d92c90f795f9172468cda75057f8e63f04a1e0e5
flyingtime <flyingtimeice@gmail.com>
2026-09-14 09:40:42 +0800

    使用flutter 3.10.6版本进行编译

--- example/android/app/src/main/kotlin/com/flutter_daemon/flutter_daemon_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/flutter_daemon/flutter_daemon_example/MainActivity.kt
@@ -2,4 +2,5 @@ package com.flutter_daemon.flutter_daemon_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
